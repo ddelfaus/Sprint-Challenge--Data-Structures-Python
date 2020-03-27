a82b86e61de89c001ccce0090e27a6e327477a16
Woober <ddelfaus@gmail.com>
2020-03-27 11:19:27 -0400

    initial commit

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,15 +8,24 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if len(self.storage) == self.capacity:
+            self.current.value = item
+            self.current = self.current.prev
+        else:
+            self.storage.add_to_head(item)
+        
+        self.current = (self.current + 1) % self.capacity
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
+        #The get method, which is provided, 
+        # returns all of the elements in the buffer in a list in their given order. 
+        # It should not return any None values in the list even if they are present in the ring buffer
+        # go the through each element with a while list < the length of the storage length
         # TODO: Your code here
 
-        return list_buffer_contents
+   
 
 # ----------------Stretch Goal-------------------
 
