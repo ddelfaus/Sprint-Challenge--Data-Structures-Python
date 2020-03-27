7447d1e96c3940d2b394108ab4551b4aa8d854b1
Woober <ddelfaus@gmail.com>
2020-03-27 16:42:35 -0400

    mvp

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -14,21 +14,55 @@ class RingBuffer:
         # else:
         #     self.storage.add_to_head(item)
         #     self.current = self.storage.tail
+
+
+        #almost working
+
+        # #start point for the ring
+        # if self.storage == 0:
+        #     self.storage.add_to_head(item)
+        #     self.current = self.storage(head)
         
-        #start point for the ring
-        if self.storage == 0:
-            self.storage.add_to_head(item)
-            self.current = self.storage(head)
-        
-        #overwrite when at cap.
-        if self.storage.length == self.capacity:
-            self.storage.remove_from_head()
-            self.storage.add_to_tail(item)
-            self.current = self.storage.tail
-        # add when not at cap. 
-        else:
-            self.storage.add_to_tail(item)
+        # #overwrite when at cap.
+        # if self.storage.length == self.capacity:
+        #     self.storage.remove_from_head()
+        #     self.storage.add_to_tail(item)
+        #     self.current = self.storage.tail
+        # # add when not at cap. 
+        # else:
+        #     self.storage.add_to_tail(item)
                    
+          #start point for the ring
+        # if self.storage == 0:
+        #     self.storage.add_to_head(item)
+        #     self.current = self.storage(head)
+        
+       
+        # elif self.storage.length == self.capacity and not self.storage.head:
+           
+        #     self.storage.remove_from_head()
+        #     self.current.add_to_tail(item)
+        # #overwrite when at cap.
+        # elif self.storage.length == self.capacity:
+        #     self.storage.remove_from_head()
+        #     self.storage.add_to_head(item)
+        #     self.current = self.storage.tail
+        # # add when not at cap. 
+        # else:
+        #     self.storage.add_to_tail(item)
+        # try number 4
+        if self.storage.length < self.capacity:
+            self.storage.add_to_tail(item)
+            if self.storage.length == self.capacity:
+                self.current = self.storage.head
+        
+        else: 
+            self.current.value = item
+            if self.current.next != None:
+                self.current = self. current.next
+
+            else: 
+                self.current = self.storage.head
         
 
     def get(self):
@@ -40,11 +74,20 @@ class RingBuffer:
        
         # TODO: Your code here
         
-        # current_item = self.storage.tail
-        # while current_item is not self.storage.head:
-        #     list_buffer_contents.append(current_item.value)
-        #     current_item = current_item.prev
-        # list_buffer_contents.append(self.storage.head.value)
+       
+        current = self.storage.head
+        while len(list_buffer_contents) < self.storage.length:
+            print(current.value, "324432")
+            list_buffer_contents.append(current.value)
+         
+            if current.next: 
+                current = current.next
+        else: 
+            current = self.storage.tail
+
+        
+        return list_buffer_contents
+
 # ----------------Stretch Goal-------------------
 
 
